8ccb8b436750936a2beee5cc444e96230d974465
Riley Winkler <rex8112@gmail.com>
2021-06-24 05:18:01 -0500

    Create InvalidConanModPathException.cs

---
+++ Exceptions/InvalidConanModPathException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConanExilesModlistManager.Exceptions
+{
+    class InvalidConanModPathException : Exception
+    {
+        public InvalidConanModPathException()
+        {
+
+        }
+        public InvalidConanModPathException(string message)
+            : base(message)
+        {
+
+        }
+        public InvalidConanModPathException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+}
